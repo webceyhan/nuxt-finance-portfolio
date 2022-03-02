ef6fecde2d10311f7ecbe7932f1074f1f1fbd0c3
Evren Ceyhan <evren@ceyhan.io>
2022-03-03 00:08:17 +0300

    api endpoints impl.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 
-import { onMounted, ref } from 'vue';
-import { getGoldPrices, GoldResponse } from './api'
-
-const list = ref<GoldResponse[]>([])
+import { onMounted, ref, computed } from 'vue';
+import { getAssets , getHoldings, Asset, Holding } from './api'
 
 const formatCurrency = (value: number) =>
   new Intl.NumberFormat('tr-TR', {
@@ -11,8 +9,21 @@ const formatCurrency = (value: number) =>
     currency: 'TRY',
   }).format(value)
 
+const assets = ref<Asset[]>([])
+const holdings = ref<Holding[]>([])
+
+const assetMap = computed<{ [key: string]: Asset }>(() => assets.value.reduce(
+  (acc, cur) => ({ ...acc, [cur.name]: cur }), {}));
+
+const portfolio = computed(() => holdings.value.map(
+  (holding) => ({ ...holding, ...assetMap.value[holding.name] })));
+
+const total = computed(() => portfolio.value.reduce(
+  (acc, cur) => acc + (cur.buying * cur.amount), 0));
+
 onMounted(async () => {
-  list.value = await getGoldPrices()
+  assets.value = await getAssets();
+  holdings.value = await getHoldings();
 });
 
 </script>
@@ -22,12 +33,29 @@ onMounted(async () => {
     <h1 class="display-3">Finance Portfolio</h1>
 
     <ul class="list-group">
-      <li class="list-group-item list-group-item-dark" v-for="item in list">
-        <div class="d-flex justify-content-between align-items-center">
-          <span>{{ item.name }}</span>
-          <span class="badge bg-dark">{{ formatCurrency(item.buying) }}</span>
+      <li class="list-group-item list-group-item-dark" v-for="holding in portfolio">
+        <div class="row align-items-center">
+          <div class="col-4">{{ holding.name }}</div>
+          <div class="col">
+            <span class="badge bg-dark">{{ formatCurrency(holding.buying) }}</span>
+          </div>
+          <span class="col-auto text-muted">x</span>
+          <div class="col text-end">
+            <span class="badge bg-dark">{{ holding.amount }}</span>
+          </div>
+          <span class="col-auto text-muted">=</span>
+          <div class="col text-end">
+            <span class="badge bg-dark">{{ formatCurrency(holding.buying * holding.amount) }}</span>
+          </div>
         </div>
       </li>
     </ul>
+
+    <hr />
+
+    <div class="row">
+      <h4 class="col">Total</h4>
+      <h4 class="col-auto">{{ formatCurrency(total) }}</h4>
+    </div>
   </div>
 </template>
--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,7 @@ const headers = {
     authorization: `apikey ${API_KEY}`,
 };
 
-export interface GoldResponse {
+export interface Asset {
     name: string;
     buying: number;
     selling: number;
@@ -14,13 +14,37 @@ export interface GoldResponse {
     rate: number;
 }
 
+export interface Holding {
+    name: string;
+    amount: number;
+    buyPrice?: number;
+    timestamp?: number;
+}
+
 const fetchApi = async (path: string) => {
     const response = await fetch(`${API_URL}${path}`, { headers });
     return response.json();
 };
 
+export const getCurrencies = async () => {
+    return (await import('./all-currency.json')).default as Asset[];
+    // const response = await fetchApi('/allCurrency');
+    // return response.result;
+};
+
 export const getGoldPrices = async () => {
-    return (await import('./goldPrice.json')).default as GoldResponse[];
+    return (await import('./gold-price.json')).default as Asset[];
     // const data = await fetchApi('/goldPrice');
+    // return data.result;
+};
+
+export const getAssets = async () => [
+    ...(await getCurrencies()),
+    ...(await getGoldPrices()),
+];
+
+export const getHoldings = async () => {
+    return (await import('./portfolio.json')).default as Holding[];
+    // const data = await fetchApi('/portfolio');
     // return data.result;
 };
