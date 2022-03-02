1fbc08a0500b928a9577ad429495a0e0ec64ec07
Evren Ceyhan <evren@ceyhan.io>
2022-03-03 00:19:58 +0300

    api functions separated

---
+++ src/api/api.ts
@@ -0,0 +1,38 @@
+import { Asset, Holding } from './models';
+
+// define api constants
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = 'https://api.collectapi.com/economy';
+const headers = {
+    'content-type': 'application/json',
+    authorization: `apikey ${API_KEY}`,
+};
+
+// define api fecther
+const fetchApi = async (path: string) => {
+    const response = await fetch(`${API_URL}${path}`, { headers });
+    return response.json();
+};
+
+export const getHoldings = async () => {
+    return (await import('./mock/portfolio.json')).default as Holding[];
+    // const data = await fetchApi('/portfolio');
+    // return data.result;
+};
+
+export const getCurrencies = async () => {
+    return (await import('./mock/all-currency.json')).default as Asset[];
+    // const response = await fetchApi('/allCurrency');
+    // return response.result;
+};
+
+export const getGoldPrices = async () => {
+    return (await import('./mock/gold-price.json')).default as Asset[];
+    // const data = await fetchApi('/goldPrice');
+    // return data.result;
+};
+
+export const getAssets = async () => [
+    ...(await getCurrencies()),
+    ...(await getGoldPrices()),
+];
