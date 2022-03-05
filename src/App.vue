efce8f137bf8c38c5564351c1f2c13391a35f036
Evren Ceyhan <evren@ceyhan.io>
2022-03-05 13:45:13 +0300

    portfolio moved to its own page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
 
-import { onMounted } from 'vue';
-import { formatCurrency } from './utils'
-import { useStore } from './store'
-
-const { load, portfolio } = useStore();
-
-onMounted(async () => load());
 
 </script>
 
@@ -18,63 +11,15 @@ onMounted(async () => load());
 
     <ul class="nav nav-tabs">
       <li class="nav-item">
-        <router-link class="nav-link" :to="{ name: 'home' }">home</router-link>
+        <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
       </li>
       <li class="nav-item">
-        <router-link class="nav-link" :to="{ name: 'portfolio' }">portfolio</router-link>
+        <router-link class="nav-link" :to="{ name: 'portfolio' }">My Portfolio</router-link>
       </li>
     </ul>
+
+    <br />
 
     <router-view></router-view>
-
-    <div class="row p-3 text-muted">
-      <div class="col-4">Name</div>
-      <div class="col">Price</div>
-      <div class="col">Amount</div>
-      <div class="col">Total</div>
-    </div>
-
-    <ul class="list-group">
-      <li class="list-group-item list-group-item-dark" v-for="holding in portfolio.holdings">
-        <div class="row align-items-center">
-          <div class="col-4">{{ holding.name }}</div>
-
-          <div class="col">
-            <span class="badge bg-dark">{{ formatCurrency(holding.price) }}</span>
-            <span class="text-muted float-end">x</span>
-          </div>
-
-          <div class="col">
-            <span class="badge bg-dark">{{ holding.amount }}</span>
-            <span class="text-muted float-end">=</span>
-          </div>
-
-          <div class="col">
-            <span class="badge bg-dark">{{ formatCurrency(holding.balance) }}</span>
-          </div>
-        </div>
-      </li>
-    </ul>
-
-    <hr />
-
-    <div class="row">
-      <h5 class="col text-muted">Cost</h5>
-      <h5 class="col-auto">{{ formatCurrency(portfolio.cost) }}</h5>
-    </div>
-
-    <hr />
-
-    <div class="row">
-      <h5 class="col text-muted">Profit</h5>
-      <h5 class="col-auto">{{ formatCurrency(portfolio.profit) }}</h5>
-    </div>
-
-    <hr />
-
-    <div class="row">
-      <h4 class="col text-muted">Total</h4>
-      <h4 class="col-auto">{{ formatCurrency(portfolio.total) }}</h4>
-    </div>
   </div>
 </template>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,9 +1,67 @@
 <script setup lang="ts">
 
+import { onMounted } from 'vue';
+import { formatCurrency } from '../utils'
+import { useStore } from '../store'
+
+const { load, portfolio } = useStore();
+
+onMounted(async () => load());
+
 </script>
 
 <template>
-    <div>
-        portfolio
-    </div>
+    <section>
+        <h1 class="display-6">My Portfolio</h1>
+
+        <div class="row p-3 text-muted">
+            <div class="col-4">Name</div>
+            <div class="col">Price</div>
+            <div class="col">Amount</div>
+            <div class="col">Total</div>
+        </div>
+
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-dark" v-for="holding in portfolio.holdings">
+                <div class="row align-items-center">
+                    <div class="col-4">{{ holding.name }}</div>
+
+                    <div class="col">
+                        <span class="badge bg-dark">{{ formatCurrency(holding.price) }}</span>
+                        <span class="text-muted float-end">x</span>
+                    </div>
+
+                    <div class="col">
+                        <span class="badge bg-dark">{{ holding.amount }}</span>
+                        <span class="text-muted float-end">=</span>
+                    </div>
+
+                    <div class="col">
+                        <span class="badge bg-dark">{{ formatCurrency(holding.balance) }}</span>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+        <hr />
+
+        <div class="row">
+            <h5 class="col text-muted">Cost</h5>
+            <h5 class="col-auto">{{ formatCurrency(portfolio.cost) }}</h5>
+        </div>
+
+        <hr />
+
+        <div class="row">
+            <h5 class="col text-muted">Profit</h5>
+            <h5 class="col-auto">{{ formatCurrency(portfolio.profit) }}</h5>
+        </div>
+
+        <hr />
+
+        <div class="row">
+            <h4 class="col text-muted">Total</h4>
+            <h4 class="col-auto">{{ formatCurrency(portfolio.total) }}</h4>
+        </div>
+    </section>
 </template>
