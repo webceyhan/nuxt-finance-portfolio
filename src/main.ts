98c13bac22c63b953b1ca1cba450c5306c267457
Evren Ceyhan <evren@ceyhan.io>
2022-03-06 16:26:29 +0300

    boostrap js files loaded

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import { router } from './router';
 import App from './App.vue';
 
-// load bootstrap styles
+// load bootstrap files
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 createApp(App).use(router).mount('#app');
