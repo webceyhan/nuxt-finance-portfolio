8224e427bded40986c446a1ddbbecc53b3234a5f
Evren Ceyhan <evren@ceyhan.io>
2022-03-14 23:46:37 +0300

    login/logout moved to App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 
+import { useAuth } from './store/auth';
+
+const { user, login, logout } = useAuth();
 
 </script>
 
@@ -16,10 +19,23 @@
       <li class="nav-item">
         <router-link class="nav-link" :to="{ name: 'portfolio' }">My Portfolio</router-link>
       </li>
+
+      <li class="nav-item ms-auto">
+        <a v-if="!user" href="#" class="nav-link" @click.prevent="login">Sign In</a>
+        <a v-else href="#" class="nav-link py-1" @click.prevent="logout">
+          <img
+            :src="(user.photoURL as any)"
+            referrerpolicy="no-referrer"
+            class="rounded-circle me-1"
+            style="height:2rem"
+          />
+          Sign Out
+        </a>
+      </li>
     </ul>
 
     <br />
 
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
