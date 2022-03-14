4461c5a9fbdb49f503f1c8447855ca84a8a4d92d
Evren Ceyhan <evren@ceyhan.io>
2022-03-15 00:18:29 +0300

    loading removed

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -5,24 +5,19 @@ import { app } from '../firebase';
 
 const auth = getAuth(app);
 const user = ref<User | null>(null);
-const loading = ref(false);
 const provider = new GoogleAuthProvider();
 
 // Listen for auth state changes
 auth.onAuthStateChanged((state) => (user.value = state));
 
 async function login() {
-    loading.value = true;
     await signInWithPopup(auth, provider);
-    loading.value = false;
 }
 
 async function logout() {
-    loading.value = true;
     await auth.signOut();
-    loading.value = false;
 }
 
 export function useAuth() {
-    return { user, loading, login, logout };
+    return { user, login, logout };
 }
