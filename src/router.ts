4f0f6e8f769c638e92b5e7f88dfd3b41b90c8ccd
Evren Ceyhan <evren@ceyhan.io>
2022-03-15 00:18:46 +0300

    auth route-guards impl.

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,9 @@
+import { watch } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import { useAuth } from './store/auth';
 import Assets from './pages/Assets.vue';
+
+const { user } = useAuth();
 
 const routes = [
     {
@@ -10,11 +14,13 @@ const routes = [
     {
         path: '/portfolio',
         name: 'portfolio',
+        meta: { requiresAuth: true },
         component: () => import('./pages/Portfolio.vue'),
     },
     {
         path: '/holdings/:id',
         name: 'holding',
+        meta: { requiresAuth: true },
         component: () => import('./pages/Holding.vue'),
     },
 ];
@@ -25,3 +31,13 @@ export const router = createRouter({
     linkActiveClass: 'active',
     linkExactActiveClass: 'active',
 });
+
+// add auth guard to all routes which require authentication
+router.beforeEach((to) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (!user.value) return { path: '/' };
+    }
+});
+
+// redirect to home page if user signed out
+watch(user, () => user.value || router.push('/'));
