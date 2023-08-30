55bf28daaff4436669a1ad693be88290a447b381
Evren Ceyhan <evren@ceyhan.io>
2023-08-30 20:14:31 +0300

    tab nav migrated

---
+++ src/components/ui/Tab.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Size } from "./types";
+
+interface Props {
+  active?: boolean;
+  lifted?: boolean;
+  bordered?: boolean;
+  disabled?: boolean;
+  size?: Size;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <a
+    :class="{
+      tab: true,
+      // state
+      'tab-active': active,
+      'tab-disabled': disabled,
+
+      // style
+      'tab-lifted': lifted,
+      'tab-bordered': bordered,
+
+      // size
+      'tab-xs': size === 'xs',
+      'tab-sm': size === 'sm',
+      'tab-md': size === 'md',
+      'tab-lg': size === 'lg',
+    }"
+  >
+    <slot />
+  </a>
+</template>
--- src/pages/Assets.vue
+++ src/pages/Assets.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from "vue";
 import { useAssets } from "../store/assets";
-import Nav from "../components/ui/Nav.vue";
-import NavLink from "../components/ui/NavLink.vue";
 import AssetList from "../components/AssetList.vue";
+import Tabs from "../components/ui/Tabs.vue";
+import Tab from "../components/ui/Tab.vue";
 
 type Tab = "fiat" | "gold";
 
@@ -28,21 +28,25 @@ onMounted(async () => load());
       <h1 class="display-5">Assets</h1>
     </header>
 
-    <Nav type="tabs">
-      <NavLink
-        label="Fiat"
+    <Tabs block>
+      <Tab
         :active="selectedTab === 'fiat'"
         @click.prevent="selectedTab = 'fiat'"
-      />
+        bordered
+      >
+        Fiat
+      </Tab>
 
-      <NavLink
-        label="Gold"
+      <Tab
         :active="selectedTab === 'gold'"
         @click.prevent="selectedTab = 'gold'"
-      />
-    </Nav>
+        bordered
+      >
+        Gold
+      </Tab>
+    </Tabs>
 
-    <br>
+    <br />
 
     <AssetList :assets="assets" />
   </div>
