11acd94bfacebc9f6fc0ecfdea374eb0abf7bf10
Evren Ceyhan <evren@ceyhan.io>
2023-08-31 12:16:30 +0300

    list group light color adjusted

--- src/components/ui/ListGroupItem.vue
+++ src/components/ui/ListGroupItem.vue
@@ -13,10 +13,10 @@ defineProps<Props>();
     :to="to"
     :class="{
       // base
-      'flex justify-between bg-neutral py-4 px-6': true,
+      'flex justify-between bg-neutral-content dark:bg-neutral py-4 px-6': true,
 
       // hover
-      'hover:bg-neutral-focus': hoverable || to,
+      'hover:bg-neutral-content/70 dark:hover:bg-neutral-focus': hoverable || to,
     }"
   >
     <slot />
