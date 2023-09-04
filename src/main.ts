9c8df05c7c7437b5e7f9bd9c45f32419139bdbf3
Evren Ceyhan <evren@ceyhan.io>
2023-09-04 22:22:20 +0300

    obsolete files removed

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import { router } from './router';
-import App from './App.vue';
-
-// load global styles
-import './styles.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-createApp(App).use(router).mount('#app');
