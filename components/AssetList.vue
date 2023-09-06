c90ca454baeed30f9d0810622d6b7c00664e255d
Evren Ceyhan <evren@ceyhan.io>
2023-09-06 18:33:48 +0300

    assetlist breaking points refactored

--- components/AssetList.vue
+++ components/AssetList.vue
@@ -17,29 +17,29 @@ const { sortAsc, sortBy, sortedData } = useSort<Asset>({
 </script>
 
 <template>
-  <div class="grid grid-cols-2 sm:grid-cols-4 py-2 px-6">
+  <div class="grid grid-cols-2 md:grid-cols-4 py-2 px-6">
     <!-- TODO: add sortable rate column which should be default -->
     <div class="">
       <SortButton value="name" v-model:by="sortBy" v-model:asc="sortAsc">
         Name
       </SortButton>
     </div>
-    <div class="max-sm:hidden text-end">
+    <div class="max-md:hidden text-end">
       <SortButton value="buying" v-model:by="sortBy" v-model:asc="sortAsc">
         Buy
       </SortButton>
     </div>
-    <div class="max-sm:hidden text-end">
+    <div class="max-md:hidden text-end">
       <SortButton value="selling" v-model:by="sortBy" v-model:asc="sortAsc" disabled>
         Sell
       </SortButton>
     </div>
-    <div class="max-sm:hidden text-end">
+    <div class="max-md:hidden text-end">
       <SortButton value="delta" v-model:by="sortBy" v-model:asc="sortAsc">
         Delta
       </SortButton>
     </div>
-    <div class="sm:hidden text-end">
+    <div class="md:hidden text-end">
       <SortButton value="buying" v-model:by="sortBy" v-model:asc="sortAsc">
         Price
       </SortButton>
@@ -53,29 +53,29 @@ const { sortAsc, sortBy, sortedData } = useSort<Asset>({
       @click="$emit('select', asset)"
       hoverable
     >
-      <div class="w-full grid grid-cols-2 sm:grid-cols-4">
-        <div class="flex max-md:flex-col gap-x-2">
+      <div class="w-full grid grid-cols-2 md:grid-cols-4">
+        <div class="flex max-md:flex-col gap-x-2 whitespace-nowrap">
           <span>{{ asset.name }}</span>
           <span class="opacity-50">{{ asset.code }}</span>
         </div>
 
-        <div class="max-sm:hidden text-end">
+        <div class="max-md:hidden text-end">
           <Money :value="asset.buying" />
         </div>
 
-        <div class="max-sm:hidden text-end">
+        <div class="max-md:hidden text-end">
           <Money :value="asset.selling" />
         </div>
 
         <div
-          class="max-sm:hidden text-end text-sm"
+          class="max-md:hidden text-end text-sm"
           :class="`text-${priceColor(asset.delta)}`"
         >
           {{ asset.delta.toFixed(2) }}%
         </div>
 
         <!-- mobile only -->
-        <div class="sm:hidden text-end">
+        <div class="md:hidden text-end">
           <Money :value="asset.buying" />
 
           <div :class="`text-sm text-${priceColor(asset.delta)}`">
