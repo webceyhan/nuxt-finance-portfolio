0009b85f2392defd55296b263656d8211c155f59
Evren Ceyhan <evren@ceyhan.io>
2023-09-15 02:41:49 +0300

    useLanguage composable added

--- composables/i18n.ts
+++ composables/i18n.ts
@@ -7,3 +7,14 @@ export function useLocale() {
 export function useCurrency() {
     return useState<Currency>('currency', () => 'TRY');
 }
+
+export function useLanguage() {
+    const { locale, setLocale } = useI18n();
+
+    watch(
+        () => locale.value,
+        (locale) => setLocale(locale)
+    );
+
+    return locale;
+}
