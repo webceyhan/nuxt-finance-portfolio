0f66c3d1b20ac3f49a2cd14b6610e8c83c147c4d
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-04-12 19:52:11 +0200

    upgrade nuxt
    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    devtools: { enabled: true },
-    modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss'],
-    components: [
-        {
-            // disable auto-prefixing of component names
-            path: '~/components',
-            pathPrefix: false,
-        },
-    ],
-    runtimeConfig: {
-        public: {
-            firebase: {
-                apiKey: '<apiKey>',
-                authDomain: '<authDomain>',
-                projectId: '<projectId>',
-                storageBucket: '<storageBucket>',
-                messagingSenderId: '<messagingSenderId>',
-                appId: '<appId>',
-            },
-        },
+  compatibilityDate: "2025-04-12",
+  ssr: false,
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss"],
+
+  components: [
+    {
+      // disable auto-prefixing of component names
+      path: "~/components",
+      pathPrefix: false,
     },
-    i18n: {
-        locales: ['en', 'tr'],
-        defaultLocale: 'en',
-        fallbackLocale: 'en',
-        detectBrowserLanguage: false,
-        skipSettingLocaleOnNavigate: true,
-        vueI18n: './i18n.config.ts',
+  ],
+
+  runtimeConfig: {
+    public: {
+      firebase: {
+        apiKey: "<apiKey>",
+        authDomain: "<authDomain>",
+        projectId: "<projectId>",
+        storageBucket: "<storageBucket>",
+        messagingSenderId: "<messagingSenderId>",
+        appId: "<appId>",
+      },
     },
+  },
+
+  i18n: {
+    locales: ["en", "tr"],
+    defaultLocale: "en",
+    fallbackLocale: "en",
+    detectBrowserLanguage: false,
+    skipSettingLocaleOnNavigate: true,
+    vueI18n: "./i18n.config.ts",
+  },
 });
+
