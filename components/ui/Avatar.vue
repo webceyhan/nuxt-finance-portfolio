e13aed17ddc77be0919aa3ce98e94e37bdea1b4d
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-04-13 20:18:51 +0200

    migrate avatar component

--- components/ui/Avatar.vue
+++ components/ui/Avatar.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+/**
+ * Dynamic classes
+ *
+ * Size:
+ *  avatar-xs
+ *  avatar-sm
+ *  avatar-md
+ *  avatar-lg
+ *  avatar-xl
+ */
 defineProps<{
   src: string;
   size?: Size;
@@ -7,17 +17,44 @@ defineProps<{
 </script>
 
 <template>
-  <div class="avatar">
+  <div
+    :class="{
+      avatar: true,
+      [`avatar-${size}`]: size,
+    }">
     <!-- bugfix: !aspect-auto added to the div to avoid the image being stretched on the first render -->
     <div
       :class="{
         'rounded-full !aspect-auto': true,
-        'w-8': size === 'sm',
-        'w-12': size === 'md' || !size,
-        'w-16': size === 'lg',
         'ring ring-primary ring-offset-base-100 ring-offset-2': withRing,
       }">
       <img :src="src" referrerpolicy="no-referrer" />
     </div>
   </div>
 </template>
+
+<style scoped>
+.avatar {
+  --size-factor: 12; /* default */
+
+  & > div {
+    width: calc(var(--spacing) * var(--size-factor));
+  }
+}
+
+.avatar-xs {
+  --size-factor: 6;
+}
+.avatar-sm {
+  --size-factor: 8;
+}
+.avatar-md {
+  --size-factor: 12; /* default */
+}
+.avatar-lg {
+  --size-factor: 16;
+}
+.avatar-xl {
+  --size-factor: 24;
+}
+</style>
