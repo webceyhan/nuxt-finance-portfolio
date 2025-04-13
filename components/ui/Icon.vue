6337d79e926482f7a176e085f2a7bcdccc98d46c
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-04-13 20:06:26 +0200

    migrate icon component

--- components/ui/Icon.vue
+++ components/ui/Icon.vue
@@ -4,6 +4,16 @@ import "bootstrap-icons/font/bootstrap-icons.min.css";
 // type hinting for bootstrap-icon names
 type Name = keyof typeof import("bootstrap-icons/font/bootstrap-icons.json");
 
+/**
+ * Dynamic classes
+ *
+ * Size:
+ *  icon-xs
+ *  icon-sm
+ *  icon-md
+ *  icon-lg
+ *  icon-xl
+ */
 defineProps<{
   name: Name;
   size?: Size;
@@ -14,9 +24,29 @@ defineProps<{
   <i
     :class="{
       [`bi bi-${name}`]: true,
-      'text-md': size === 'sm',
-      'text-lg': size === 'md' || !size,
-      'text-2xl': size === 'lg',
-      'text-4xl': size === 'xl',
+      [`icon-${size}`]: size,
     }" />
 </template>
+
+<style scoped>
+.bi {
+  --size-factor: 1.25;
+  font-size: calc(var(--text-base) * var(--size-factor));
+}
+
+.icon-xs {
+  --size-factor: 0.75;
+}
+.icon-sm {
+  --size-factor: 1;
+}
+.icon-md {
+  --size-factor: 1.25; /* default */
+}
+.icon-lg {
+  --size-factor: 1.5;
+}
+.icon-xl {
+  --size-factor: 2;
+}
+</style>
